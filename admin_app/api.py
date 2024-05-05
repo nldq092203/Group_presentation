52e19c6f957b44ca74549274d267e4ba9185e712
nldq092203 <le_diem_quynh.nguyen@insa-cvl.fr>
2024-05-05 18:28:13 +0200

    Admin

--- admin_app/api.py
+++ admin_app/api.py
@@ -5,6 +5,8 @@ from user_app.models import Member
 from user_app.serializers import MemberSerializer
 from django.shortcuts import get_object_or_404
 from user_app.api import BaseMemberDetailAPIView
+from django.utils.decorators import method_decorator
+from rest_framework import status
 
 def admin_required(function=None, redirect_field_name=None, login_url='login'):
     """
@@ -20,18 +22,39 @@ def admin_required(function=None, redirect_field_name=None, login_url='login'):
         return actual_decorator(function)
     return actual_decorator
 
-@admin_required
+
 class MemberListAPIView(ListAPIView):
     queryset = Member.objects.filter(is_member=True)
     serializer_class = MemberSerializer
-
+    @method_decorator(admin_required(login_url='login'))
     def get(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         serializer = self.get_serializer(queryset, many=True)
         return Response({'serializer': serializer.data})
     
+   
 class MemberManageAPIView(BaseMemberDetailAPIView, DestroyAPIView):
+    @method_decorator(admin_required(login_url='login'))
     def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+        self.destroy(request, *args, **kwargs)
+        return Response({"status": "success"}, status=status.HTTP_204_NO_CONTENT)
+    
+    @method_decorator(admin_required(login_url='login'))
+    def put(self, request, format=None):
+        return super().put(request, format)
+    
+    @method_decorator(admin_required(login_url='login'))
+    def post(self, request, format=None):
+        return super().post(request, format)
 
-     
+class RequestListAPIView(ListAPIView):
+    queryset = Member.objects.filter(is_requested = True, is_member = False)
+    serializer_class = MemberSerializer
+
+    @method_decorator(admin_required(login_url='login'))
+    def get(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'serializer': serializer.data})
+
+
